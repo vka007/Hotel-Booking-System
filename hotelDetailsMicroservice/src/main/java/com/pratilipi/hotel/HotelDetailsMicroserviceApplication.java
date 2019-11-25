bd56280a76860e96f82dd7770172ec44bb4064c6
vka007 <vineet.agrawal100@gmail.com>
2019-11-25 21:44:54 +0530

    Hotel Details Microservice Initialized

---
+++ hotelDetailsMicroservice/src/main/java/com/pratilipi/hotel/HotelDetailsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pratilipi.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelDetailsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelDetailsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ hotelDetailsMicroservice/src/test/java/com/pratilipi/hotel/HotelDetailsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratilipi.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelDetailsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
